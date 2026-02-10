fa96da5ba25309e4f696e8107d66a3c24ebe7177
Viuviu42 <lengyelcsongi@gmail.com>
2026-02-10 10:25:24 +0100

    view model nevek angol + ipari komment megalkotás

--- WMS_Projekt/EmployeeViewModell.cs
+++ WMS_Projekt/EmployeeViewModell.cs
@@ -1,9 +1,21 @@
 ﻿namespace WMS_Projekt
 {
+    /// <summary>
+    /// Dolgozó adatok megjelenítéshez (szabadnapkezelés)
+    /// </summary>
     public class EmployeeViewModell
     {
+        /// <summary>
+        /// Dolgozó neve
+        /// </summary>
         public string Name { get; } = "Nagy Hanna";
-        public string Adószám { get; } = "1112223334";
-        public int SzabadNapok { get; } = 3;
+        /// <summary>
+        /// Adószám
+        /// </summary>
+        public string TaxNumber { get; } = "1112223334";
+        /// <summary>
+        /// Kivett szabadnapok száma
+        /// </summary>
+        public int UsedDaysOff { get; } = 3;
     }
 }
